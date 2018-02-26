18ba1fa8c2fd4f06cd0eda23e0c2442b8884e6d5
Estudiantes <Estudiantes@Labing70206>
2018-02-26 17:23:45 -0500

    Guardar en miMoto

---
+++ Principal/src/principal/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import Parqueadero.FichaVehiculo;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        FichaVehiculo miFicha=new FichaVehiculo();
+        miFicha.setVisible(true);
+    }
+    
+}
